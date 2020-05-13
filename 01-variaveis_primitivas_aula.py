17e012b068c7a3e88720c84a043c0852377af615
Leonardo <demetrio.vd@hotmail.com>
2020-05-13 19:25:42 -0300

    Acelera DEV Python codenation, variaveis primitivas

---
+++ 01-variaveis_primitivas_aula.py
@@ -0,0 +1,27 @@
+nome = input('Digite seu nome')
+idade = int(input('Digite sua idade'))
+altura = float(input('Digite sua altura'))
+tutor = False
+trabalhando = None
+
+print(nome)
+print(idade)
+print(altura)
+print(tutor)
+print(trabalhando)
+
+
+nova_idade = idade + 30
+
+nome_cidade = nome + ' São Paulo'
+
+print("Nova idade : " , nova_idade)
+print("Nome e cidade : ", nome_cidade)
+
+altura_sem_tenis = altura - 2.0
+
+print("Altura sem tênis : ", altura_sem_tenis)
+
+resultado = idade + altura
+
+print(resultado)
